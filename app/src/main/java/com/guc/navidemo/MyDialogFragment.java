b004c9d5f9683c0cfbdb3af320d21586549d2062
guchaochao <guchaochao@huiyunit.com>
2019-10-24 16:59:39 +0800

    DialogFragment

---
+++ app/src/main/java/com/guc/navidemo/MyDialogFragment.java
@@ -0,0 +1,22 @@
+package com.guc.navidemo;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by guc on 2019/10/24.
+ * 描述：
+ */
+public class MyDialogFragment extends DialogFragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_fragment, container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/guc/navidemo/SecondFragment.java
+++ app/src/main/java/com/guc/navidemo/SecondFragment.java
@@ -7,16 +7,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by guc on 2019/10/23.
  * 描述：
  */
 public class SecondFragment extends Fragment {
+    private Button mBtnNext;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_second,container,false);
+        mBtnNext = view.findViewById(R.id.next_btn);
+        mBtnNext.setOnClickListener((v) -> {
+            MyDialogFragment dialogFragment = new MyDialogFragment();
+            dialogFragment.show(getActivity().getSupportFragmentManager(), "dialog");
+        });
         return view;
     }
 }
